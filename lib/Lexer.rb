a8c33136bd9b830b1fea596547c6a39f83143046
Robert Curth <robert@rocu.de>
2011-10-30 15:34:04 +0100

    Renamed methods in Lexer

--- lib/Lexer.rb
+++ lib/Lexer.rb
@@ -5,23 +5,23 @@ class Lexer
 
   def tokenize(code)
     @tokens = []
-    code.each_line {|l| parse_single_line(l)}
+    code.each_line {|l| parse_line(l)}
     @tokens
   end
 
-  def parse_single_line(line)
-    check_newline(line)
-    process_indentation(line)
-    line.split(/ /).each{|chunk| tokenize_single_bit(chunk)}
+  def parse_line(line)
+    tokenize_newlines(line)
+    tokenize_indentation(line)
+    line.split(/ /).each{|chunk| tokenize_chunk(chunk)}
   end
 
-  def check_newline(line)
+  def tokenize_newlines(line)
     if /^[\n]+/.match(line)
       @tokens << [:NEWLINE, "\n"]
     end
   end
 
-  def process_indentation(line)
+  def tokenize_indentation(line)
     line_indentation = 0
 
     if leading_whitespace = /^([ ]+)/.match(line)
@@ -33,13 +33,15 @@ class Lexer
     @tokens << [:DEDENT, @prev_indentation - line_indentation] if line_indentation < @prev_indentation
   end
 
-  def tokenize_single_bit(chunk)
+  def tokenize_chunk(chunk)
     if /if/.match(chunk)
       @tokens << [:IF, "if"]
     elsif /[0-9]+/.match(chunk)
       @tokens << [:NUMBER, chunk.to_i]
     elsif string = /\"(.*)\"/.match(chunk)
       @tokens << [:STRING, string[1]]
+    elsif /[a-z]+/.match(chunk)
+      @tokens << [:IDENTIFIER, chunk]
     end
   end
 end
