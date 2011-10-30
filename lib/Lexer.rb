9be9e672a44b2d38d5fd18ed2ada8f4ac96827f1
Robert Curth <robert@rocu.de>
2011-10-30 13:22:27 +0100

    Minor additions to the lexer
    - Added (primitive) support for indentation / dedentation
    - Added support for matching newlines
    - Added one test that fails as a reminder what to do next

--- lib/Lexer.rb
+++ lib/Lexer.rb
@@ -1,15 +1,45 @@
 class Lexer
+  def initialize
+    @prev_indentation = 0
+  end
+
   def tokenize(code)
     @tokens = []
-    code.split(/ /).each{|chunk| tokenize_single_bit(chunk)}
-    return @tokens
+    code.each_line {|l| parse_single_line(l)}
+    @tokens
+  end
+
+  def parse_single_line(line)
+    check_newline(line)
+    process_indentation(line)
+    line.split(/ /).each{|chunk| tokenize_single_bit(chunk)}
+  end
+
+  def check_newline(line)
+    if /^[\n]+/.match(line)
+      @tokens << [:NEWLINE, "\n"]
+    end
+  end
+
+  def process_indentation(line)
+    line_indentation = 0
+
+    if leading_whitespace = /^([ ]+)/.match(line)
+      line_indentation = leading_whitespace.length
+      @tokens << [:INDENT, line_indentation - @prev_indentation] if line_indentation > @prev_indentation
+      @prev_indentation = line_indentation
+    end
+
+    @tokens << [:DEDENT, @prev_indentation - line_indentation] if line_indentation < @prev_indentation
   end
 
   def tokenize_single_bit(chunk)
-    if chunk == "if"
+    if /if/.match(chunk)
       @tokens << [:IF, "if"]
-    elsif chunk.match /[0-9]+/
+    elsif /[0-9]+/.match(chunk)
       @tokens << [:NUMBER, chunk.to_i]
+    elsif string = /\"(.*)\"/.match(chunk)
+      @tokens << [:STRING, string[1]]
     end
   end
 end
--- spec/Lexer_spec.rb
+++ spec/Lexer_spec.rb
@@ -7,16 +7,41 @@ describe "Lexer" do
     lexer.tokenize("").should eql([])
   end
 
-  it "can parse an if" do
+  it "can parse keywords" do
     lexer.tokenize("if").should eql([[:IF, "if"]])
   end
 
+  it "can parse identifiers" do
+    lexer.tokenize("echo 1").should eql([[:IDENTIFIER, "echo"], [:NUMBER, 1]])
+  end
+
   it "can parse a number" do
-    lexer.tokenize("1").should eql([[:NUMBER, 1]])
     lexer.tokenize("45464").should eql([[:NUMBER, 45464]])
   end
 
-  it "can parse multiple tokens" do
-    lexer.tokenize("if 1").should eql([[:IF, "if"],[:NUMBER, 1]])
+  it "can parse a string" do
+    lexer.tokenize("\"hello\"").should eql([[:STRING, "hello"]])
+  end
+
+  it "can parse newlines" do
+    lexer.tokenize("1
+
+1").should eql([[:NUMBER, 1],[:NEWLINE, "\n"],[:NUMBER, 1]])
+  end
+
+  it "can parse an indentation" do
+     code =
+'if 1:
+  1'
+    lexer.tokenize(code).should eql([[:IF, "if"],[:NUMBER, 1],[:INDENT, 2],[:NUMBER, 1]])
+  end
+
+  it "can parse a dedentation" do
+     code =
+'if 1:
+  1
+1'
+    lexer.tokenize(code).should eql(
+       [[:IF, "if"],[:NUMBER, 1],[:INDENT, 2],[:NUMBER, 1],[:DEDENT, 2], [:NUMBER, 1]])
   end
 end
