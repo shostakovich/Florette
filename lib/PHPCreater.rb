f539859ae47fe3766c53e351d989a3f4c80a6efc
Robert Curth <robert@rocu.de>
2011-11-01 17:39:13 +0100

    Introduced PHPCreater
    - Adds php-tags
    - Has very simple support for function calls now

--- lib/PHPCreater.rb
+++ lib/PHPCreater.rb
@@ -1,3 +1,27 @@
 class PHPCreater
-  # To change this template use File | Settings | File Templates.
+  def initialize(nodes)
+    @nodes = nodes
+  end
+
+  def create
+    @code = ""
+    open_php_tag
+    process_nodes
+    close_php_tag
+    @code
+  end
+
+  def process_nodes
+    @nodes.nodes.each do |node|
+      @code += "\n#{node.identifier}();"
+    end
+  end
+
+  def open_php_tag
+    @code += "<?php\n"
+  end
+
+  def close_php_tag
+    @code += "\n?>"
+  end
 end
--- spec/PHPCreater_spec.rb
+++ spec/PHPCreater_spec.rb
@@ -1,10 +1,40 @@
 require "rspec"
+require "PHPCreater"
+require "Nodes"
+require "CallNode"
 
 describe "PHPCreater" do
-
   it "should be initialized with nodes" do
+    nodes = Nodes.new "foo"
+    PHPCreater.new(nodes)
+  end
 
-    #To change this template use File | Settings | File Templates.
-    true.should == false
+  context "create" do
+    it "should generate prepend <?php" do
+      nodes = Nodes.new []
+      creater = PHPCreater.new(nodes)
+      creater.create.should match(/^<\?php/)
+    end
+
+    it "should append ?>" do
+      nodes = Nodes.new []
+      creater = PHPCreater.new(nodes)
+      creater.create.should match(/\?>/)
+    end
+
+    it "should return a php function call for a simple call node" do
+       nodes = Nodes.new([CallNode.new("exit")])
+
+       creater = PHPCreater.new(nodes)
+       creater.create.should match /^exit\(\);$/
+    end
+
+    it "should handle multiple function calls" do
+       nodes = Nodes.new([CallNode.new("exit"), CallNode.new("phpinfo")])
+
+       creater = PHPCreater.new(nodes)
+       creater.create.should match /^exit\(\);$/
+       creater.create.should match /^phpinfo\(\);$/
+    end
   end
 end
