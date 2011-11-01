0389232261f65add47e4f9ac0aec3d0bd48f08c3
Robert Curth <robert@rocu.de>
2011-11-01 16:51:28 +0100

    Removed includes from spec helper and moved them to specs

--- spec/Parser_spec.rb
+++ spec/Parser_spec.rb
@@ -1,9 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require "Parser"
 
 describe "Parser" do
   let(:parser) {Parser.new }
 
   it "empty string should have main node" do
-    parser.parse("").nodes().length.should eql(0)
+    parser.parse("")
+  end
+
+  it "parses a literal node" do
+    parser.parse("1")
+  end
+
+  it "parses an if statement do" do
+    parser.parse("echo")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'rspec'
-require 'Florette'
 require 'Lexer'
-require 'Parser'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
